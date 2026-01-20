aa2821336eeaa0980a136bf4c85c512b84478945
hayeonkim0418 <72956163+hayeonkim0418@users.noreply.github.com>
2026-01-20 18:46:12 +0900

    progressBar 추가
    검사 상단 잔여시간 타이머

---
+++ resource/js/ProgressBar.js
@@ -0,0 +1,32 @@
+export const ProgressBar = () => {
+  const totalTime = 10; // 설정할 총 시간 (초)
+  let timeLeft = totalTime;
+
+  const secondsElement = document.getElementById("seconds");
+  const progressCircle = document.querySelector(".timer-progress");
+  const radius = 45;
+  const circumference = 2 * Math.PI * radius; // 원의 둘레: 약 282.7
+
+  // 초기화
+  progressCircle.style.strokeDasharray = circumference;
+
+  function updateTimer() {
+    // 숫자 업데이트
+    secondsElement.innerText = timeLeft;
+
+    // 프로그레스 바 업데이트
+    const offset = circumference - (timeLeft / totalTime) * circumference;
+    progressCircle.style.strokeDashoffset = offset;
+
+    if (timeLeft > 0) {
+      timeLeft--;
+    } else {
+      clearInterval(timerInterval);
+      // alert("시간이 종료되었습니다!");
+    }
+  }
+
+  // 1초마다 실행
+  updateTimer(); // 즉시 한 번 실행
+  const timerInterval = setInterval(updateTimer, 1000);
+};
--- resource/js/global.js
+++ resource/js/global.js
@@ -1,5 +1,6 @@
 import { Header } from "./Header.js";
 // import { Password } from "./Password.js";
+import { ProgressBar } from "./ProgressBar.js";
 import { TableTextScroll } from "./TableTextScroll.js";
 import { SelectEvent } from "./SelectEvent.js";
 import { SelectPerson } from "./SelectPerson.js";
@@ -8,6 +9,7 @@ import { initCustomChart } from "./initCustomChart.js";
 import { initCustomChartNoData } from "./initCustomChart_NoData.js";
 
 Header();
+ProgressBar();
 TableTextScroll();
 SelectEvent();
 SelectPerson();
